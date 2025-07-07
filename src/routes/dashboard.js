99a8c07c4b3f40ba46b04aeb3db56934c5107f3f
Xrivera27 <griveram27@gmail.com>
2025-07-07 15:59:32 -0600

    se hicieron cambios esteticos

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboardController');
 const superDashboardController = require('../controllers/superDashboardController');
-const vendedorDashboardController = require('../controllers/vendedorDashboardController');
+const vendedorDashboardController = require('../controllers/VendedorDashboardController');
 const { 
   authenticateToken, 
   requireAdmin, 
